9ca53cbdd69c2efcf1671f8c6bbc5e68d270eed8
Michael Koller <koller_michael@outlook.de>
2023-12-16 18:08:41 +0100

    update copy_to_factorio.ps1

--- copy_to_factorio.ps1
+++ copy_to_factorio.ps1
@@ -12,17 +12,21 @@ $files = (
     "lib.lua"
 )
 
+Write-Host "source: $source\"
+
 foreach ($folder in $folders) {
     if ("$destination\$folder" -ne $null) {
         Remove-Item "$destination\$folder" -Force -Recurse
     }
-    New-Item -Path $destination -Name $folder -Type "directory" -Force
-    Copy-Item -Path "$source\$folder\*" -Destination "$destination\$folder" -Recurse -Verbose -Force
+    $null = New-Item -Path $destination -Name $folder -Type "directory" -Force
+    Copy-Item -Path "$source\$folder\*" -Destination "$destination\$folder" -Recurse -Force
+    Write-Host "destination: $destination\$folder\*"
 }
 
 foreach ($file in $files) {
     if ("$destination\$file" -ne $null) {
         Remove-Item "$destination\$file" -Force
     }
-    Copy-Item -Path "$source\$file" -Destination "$destination\$file" -Recurse -Verbose -Force
+    Copy-Item -Path "$source\$file" -Destination "$destination\$file" -Recurse -Force
+    Write-Host "destination: $destination\$file"
 }
