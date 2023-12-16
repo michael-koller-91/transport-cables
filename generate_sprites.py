0adff67441a1303a8167af030c32271a0cb3a741
Michael Koller <koller_michael@outlook.de>
2023-12-16 18:52:16 +0100

    update generate_sprites.py

--- generate_sprites.py
+++ generate_sprites.py
@@ -11,13 +11,21 @@ parser.add_argument(
 )
 parargs = parser.parse_args()
 
-sprites = Path("sprites")
-n_tiers = 3
 rng = np.random.default_rng(123456789)
+
+sprites = Path("sprites")
+
+n_tiers = 2
+pixels = 64
+pixels_shadow = 70
+pixels_space = 20
+thickness = 10
+
 images = {
     "array": list(),
     "filename": list(),
 }
+
 
 #
 # create the sprites directory if it does not exist
@@ -29,10 +37,107 @@ if not sprites.exists():
 
 
 #
+# placeholder
+#
+arr = np.zeros((32, 32, 3), dtype=np.uint8)
+arr[10:-10, 10:-10, 0] = 255
+arr[10:-10, 10:-10, 2] = 255
+
+images["array"].append(arr)
+images["filename"].append("low-res-placeholder.png")
+
+arr = np.ones((48, 48, 3), dtype=np.uint8) * 255
+arr[:, :, 1] = 0
+arr[3:-3, 3:-3, :] = 0
+
+images["array"].append(arr)
+images["filename"].append("low-res-placeholder-shadow.png")
+
+
+#
 # provider
 #
 for t in range(1, n_tiers + 1):
-    border = 20
+    thin_border = 2
+    arr = np.zeros((pixels, pixels, 3), dtype=np.uint8)
+    arr[:, :, 1] = 255
+    for k in range(1, 2 * t):
+        arr[
+            k * thin_border : -k * thin_border,
+            k * thin_border : -k * thin_border,
+            1,
+        ] = np.uint8(
+            np.round(255 * np.cos(np.pi / 2 * k) ** 2)
+        )  # alternate between black and green
+
+    arr[
+        pixels // 2 - thickness : pixels // 2 + thickness,
+        pixels // 2 - thickness : pixels // 2 + thickness,
+        2,
+    ] = 255
+
+    images["array"].append(arr)
+    images["filename"].append(f"provider-t{t}.png")
+
+    #
+    arr = np.zeros((pixels_shadow, pixels_shadow, 3), dtype=np.uint8)
+
+    images["array"].append(arr)
+    images["filename"].append(f"provider-t{t}-shadow.png")
+
+
+#
+# requester
+#
+for t in range(1, n_tiers + 1):
+    thin_border = 2
+    super_arr = np.zeros((pixels, 4 * pixels + 3 * pixels_space, 4), dtype=np.uint8)
+    for i in range(4):
+        arr = np.zeros((pixels, pixels, 4), dtype=np.uint8)
+        arr[:, :, -1] = 255
+        arr[:, :, 1] = 255
+        for k in range(1, 2 * t):
+            arr[
+                k * thin_border : -k * thin_border,
+                k * thin_border : -k * thin_border,
+                1,
+            ] = np.uint8(
+                np.round(255 * np.cos(np.pi / 2 * k) ** 2)
+            )  # alternate between black and green
+
+        arr[
+            : pixels // 2 + thickness,
+            pixels // 2 - thickness : pixels // 2 + thickness,
+            0,
+        ] = 255
+        arr = np.rot90(arr, k=i * 3)
+
+        super_arr[
+            :, i * pixels + i * pixels_space : (i + 1) * pixels + i * pixels_space, :
+        ] = arr
+
+    images["array"].append(super_arr)
+    images["filename"].append(f"requester-t{t}.png")
+
+    #
+    super_arr = np.zeros((pixels_shadow, 4 * pixels_shadow + 3 * pixels_space, 4), dtype=np.uint8)
+    for i in range(4):
+        arr = np.zeros((pixels_shadow, pixels_shadow, 4), dtype=np.uint8)
+        arr[:, :, -1] = 255
+
+        super_arr[
+            :, i * pixels_shadow + i * pixels_space : (i + 1) * pixels_shadow + i * pixels_space, :
+        ] = arr
+    arr = np.zeros((pixels_shadow, pixels_shadow, 3), dtype=np.uint8)
+
+    images["array"].append(super_arr)
+    images["filename"].append(f"requester-t{t}-shadow.png")
+
+
+#
+# requester-container
+#
+for t in range(1, n_tiers + 1):
     thin_border = 2
     pixels = 64
     arr = np.zeros((pixels, pixels, 3), dtype=np.uint8)
@@ -46,23 +151,29 @@ for t in range(1, n_tiers + 1):
             np.round(255 * np.cos(np.pi / 2 * k) ** 2)
         )  # alternate between black and green
 
-    arr[border:-border, border:-border, 1] = 0
-    arr[border:-border, border:-border, 2] = rng.integers(
-        100, 256, (pixels - 2 * border, pixels - 2 * border)
-    )
+    arr[
+        pixels // 2 - thickness : pixels // 2 + thickness,
+        pixels // 2 - thickness : pixels // 2 + thickness,
+        0,
+    ] = 255
 
     images["array"].append(arr)
-    images["filename"].append(f"provider-t{t}.png")
+    images["filename"].append(f"requester-container-t{t}.png")
 
     #
-    arr = np.zeros((70, 70, 3), dtype=np.uint8)
+    arr = np.zeros((pixels_shadow, pixels_shadow, 3), dtype=np.uint8)
 
     images["array"].append(arr)
-    images["filename"].append(f"provider-t{t}-shadow.png")
+    images["filename"].append(f"requester-container-t{t}-shadow.png")
 
 
 #
 # save all images
 #
 for array, filename in zip(images["array"], images["filename"]):
-    Image.fromarray(array, mode="RGB").save(sprites / filename)
+    if array.shape[-1] == 3:
+        Image.fromarray(array, mode="RGB").save(sprites / filename)
+    elif array.shape[-1] == 4:
+        Image.fromarray(array, mode="RGBA").save(sprites / filename)
+    else:
+        raise ValueError("Unknown: array.shape[-1] =", array.ndim)
