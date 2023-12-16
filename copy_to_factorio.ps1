cf0f89403dbfab8ba1b51955ea419b91aa9e4b2f
Michael Koller <koller_michael@outlook.de>
2023-12-16 16:59:58 +0100

    update copy_to_factorio.ps1

--- copy_to_factorio.ps1
+++ copy_to_factorio.ps1
@@ -1,15 +1,22 @@
 $source = "C:\git\transport-cables"
 $destination = "C:\Users\Admin\AppData\Roaming\Factorio\mods\transport-cables_0.0.1"
+$folders = (
+    "locale",
+    "prototypes",
+    "sprites"
+)
 $files = (
-    "locale\",
-    "prototypes\",
-    "sprites\",
     "control.lua",
     "data.lua",
     "info.json",
     "lib.lua"
 )
 
+foreach ($folder in $folders) {
+    New-Item -Path $destination -Name $folder -Type "directory" -Force
+    Copy-Item -Path "$source\$folder\*" -Destination "$destination\$folder" -Recurse -Verbose -Force
+}
+
 foreach ($file in $files) {
-    Copy-Item "$source\$file" -Destination "$destination\$file" -Recurse -Force -Verbose
+    Copy-Item -Path "$source\$file" -Destination "$destination\$file" -Recurse -Verbose -Force
 }
