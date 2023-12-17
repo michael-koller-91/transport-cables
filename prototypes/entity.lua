989cac1aa916abffe0d9676beada5a185ca816e1
Michael Koller <koller_michael@outlook.de>
2023-12-17 17:47:43 +0100

    update entity.lua: use new cable sprites

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -9,22 +9,20 @@ local tint = {
 }
 local belt_animation_set = {
     animation_set = {
-        filename = "__base__/graphics/entity/transport-belt/transport-belt.png",
+        filename = "__transport-cables__/sprites/hr-cable-t1.png",
         priority = "extra-high",
         width = 64,
         height = 64,
         frame_count = 16,
         direction_count = 20,
-        tint = tint,
         hr_version = {
-            filename = "__base__/graphics/entity/transport-belt/hr-transport-belt.png",
+            filename = "__transport-cables__/sprites/hr-cable-t1.png",
             priority = "extra-high",
             width = 128,
             height = 128,
             scale = 0.5,
             frame_count = 16,
             direction_count = 20,
-            tint = tint
         }
     }
 }
