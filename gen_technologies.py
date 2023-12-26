b5c645c24b1747f13c2dce5d46ad514ba25f95db
Michael Koller <koller_michael@outlook.de>
2023-12-26 19:54:48 +0100

    add tier 1 speed upgrade technology

--- gen_technologies.py
+++ gen_technologies.py
@@ -6,6 +6,7 @@ PIXELS = 64
 THICKNESS = 16
 TIER_FRAME_THICKNESS = 2
 TIERS = 2
+UPSCALE = 4
 YELLOW_LINE_OFFSET = 32
 
 images = {
@@ -15,25 +16,21 @@ images = {
 
 
 def gen(folder):
-    for tier in range(TIERS):
-        #
-        # tier 1
-        #
-        for tier in range(1, TIERS + 1):
-            arr = gu.shifted_base_cable(0, PIXELS, THICKNESS, YELLOW_LINE_OFFSET)
-            arr = gu.make_tier_lines(
-                arr,
-                tier,
-                TIER_FRAME_THICKNESS,
-                left=False,
-                top=True,
-                right=False,
-                bottom=True,
-            )
-            super_arr = gu.make_mipmaps_rgb(arr, 4)
+    for tier in range(1, TIERS + 1):
+        arr = gu.shifted_base_cable(0, PIXELS, THICKNESS, YELLOW_LINE_OFFSET)
+        arr = gu.make_tier_lines(
+            arr,
+            tier,
+            TIER_FRAME_THICKNESS,
+            left=False,
+            top=True,
+            right=False,
+            bottom=True,
+        )
+        super_arr = gu.make_mipmaps_rgb(arr, 4)
 
-            images["array"].append(super_arr)
-            images["filename"].append(f"t{tier}.png")
+        images["array"].append(super_arr)
+        images["filename"].append(f"t{tier}.png")
 
     #
     # save all images
@@ -42,6 +39,11 @@ def gen(folder):
         if array.shape[-1] == 3:
             Image.fromarray(array, mode="RGB").save(folder / filename)
         elif array.shape[-1] == 4:
-            Image.fromarray(array, mode="RGBA").save(folder / filename)
+            img = Image.fromarray(array, mode="RGBA")
+            img = img.resize(
+                (img.size[0] * UPSCALE, img.size[1] * UPSCALE), Image.Resampling.NEAREST
+            )
+            img.save(folder / filename)
+
         else:
             raise ValueError("Unknown: array.shape[-1] =", array.ndim)
--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -1,39 +1,70 @@
 local prefix = "transport-cables:"
 
-data:extend({ {
-    type = "technology",
-    name = prefix .. "t1",
-    icon_size = 64,
-    icon_mipmaps = 4,
-    icon = "__transport-cables__/sprites/technologies/t1.png",
-    effects =
+--
+-- tier 1
+--
+---------------------------------------------------------------------------
+data:extend({
     {
+        type = "technology",
+        name = prefix .. "t1",
+        icon_size = 256,
+        icon_mipmaps = 4,
+        icon = "__transport-cables__/sprites/technologies/t1.png",
+        effects =
         {
-            type = "unlock-recipe",
-            recipe = prefix .. "cable-t1"
+            {
+                type = "unlock-recipe",
+                recipe = prefix .. "cable-t1"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = prefix .. "node-t1"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = prefix .. "provider-t1"
+            },
+            {
+                type = "unlock-recipe",
+                recipe = prefix .. "requester-t1",
+            },
+            {
+                type = "unlock-recipe",
+                recipe = prefix .. "underground-cable-t1",
+            }
         },
+        unit =
         {
-            type = "unlock-recipe",
-            recipe = prefix .. "node-t1"
+            count = 50,
+            ingredients = { { "automation-science-pack", 1 } },
+            time = 15
         },
-        {
-            type = "unlock-recipe",
-            recipe = prefix .. "provider-t1"
-        },
-        {
-            type = "unlock-recipe",
-            recipe = prefix .. "requester-t1",
-        },
-        {
-            type = "unlock-recipe",
-            recipe = prefix .. "underground-cable-t1",
-        }
+        order = "a-f-a"
     },
-    unit =
     {
-        count = 50,
-        ingredients = { { "automation-science-pack", 1 } },
-        time = 15
-    },
-    order = "a-f-a"
-} })
+        type = "technology",
+        name = prefix .. "t1-speed1",
+        icons = util.technology_icon_constant_movement_speed("__transport-cables__/sprites/technologies/t1.png"),
+        effects =
+        {
+            {
+                type = "nothing",
+                effect_description = "+ 15 items / s"
+            }
+        },
+        prerequisites = { prefix .. "t1" },
+        unit =
+        {
+            count = 250,
+            ingredients =
+            {
+                { "automation-science-pack", 1 },
+                { "logistic-science-pack",   1 }
+            },
+            time = 30
+        },
+        upgrade = true,
+        order = "c-m-b"
+    }
+})
