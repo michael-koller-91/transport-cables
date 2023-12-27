848fd31472823ce2dc1694d228e92b3c3fea8548
Michael Koller <koller_michael@outlook.de>
2023-12-27 16:15:16 +0100

    update item.lua: cleanup

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,88 +1,90 @@
+local prefix = "transport-cables:"
+local entity_name = ""
 local tiers = 3
 
 for tier = 1, tiers do
     --
     -- cable
     --
-    ---------------------------------------------------------------------------
-    local entity_name = "transport-cables:cable-t" .. tostring(tier)
-    local item = table.deepcopy(data.raw["item"]["transport-belt"])
-    item.name = entity_name
-    item.icons = { { icon = "__transport-cables__/sprites/icons/cable-t" .. tostring(tier) .. ".png" } }
-    item.icon_size = 64
-    item.icon_mipmaps = 4
-    item.place_result = entity_name
-    item.group = "logistics"
-    item.subgroup = "transport-cables"
-    item.order = "a-t" .. tostring(tier)
-
-    data:extend({ item })
+    entity_name = prefix .. "cable-t" .. tostring(tier)
+    data:extend({ {
+        type = "item",
+        name = entity_name,
+        icon = "__transport-cables__/sprites/icons/cable-t" .. tostring(tier) .. ".png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+        place_result = entity_name,
+        group = "logistics",
+        subgroup = "transport-cables",
+        order = "a-t" .. tostring(tier),
+        stack_size = 100
+    } })
 
     --
     -- node
     --
-    ---------------------------------------------------------------------------
-    local entity_name = "transport-cables:node-t" .. tostring(tier)
-    local item = table.deepcopy(data.raw["item"]["splitter"])
-    item.name = entity_name
-    item.icons = { { icon = "__transport-cables__/sprites/icons/node-t" .. tostring(tier) .. ".png" } }
-    item.icon_size = 64
-    item.icon_mipmaps = 4
-    item.place_result = entity_name
-    item.group = "logistics"
-    item.subgroup = "transport-cables"
-    item.order = "c-t" .. tostring(tier)
-
-    data:extend({ item })
+    entity_name = prefix .. "node-t" .. tostring(tier)
+    data:extend({ {
+        type = "item",
+        name = entity_name,
+        icon = "__transport-cables__/sprites/icons/node-t" .. tostring(tier) .. ".png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+        place_result = entity_name,
+        group = "logistics",
+        subgroup = "transport-cables",
+        order = "c-t" .. tostring(tier),
+        stack_size = 50,
+    } })
 
     --
     -- provider
     --
-    ---------------------------------------------------------------------------
-    local entity_name = "transport-cables:provider-t" .. tostring(tier)
-    local item = table.deepcopy(data.raw["item"]["iron-chest"])
-    item.name = entity_name
-    item.icons = { { icon = "__transport-cables__/sprites/icons/provider-t" .. tostring(tier) .. ".png" } }
-    item.icon_size = 64
-    item.icon_mipmaps = 4
-    item.place_result = entity_name
-    item.group = "logistics"
-    item.subgroup = "transport-cables"
-    item.order = "d-t" .. tostring(tier)
-
-    data:extend({ item })
+    entity_name = prefix .. "provider-t" .. tostring(tier)
+    data:extend({ {
+        type = "item",
+        name = entity_name,
+        icon = "__transport-cables__/sprites/icons/provider-t" .. tostring(tier) .. ".png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+        place_result = entity_name,
+        group = "logistics",
+        subgroup = "transport-cables",
+        order = "d-t" .. tostring(tier),
+        stack_size = 50
+    } })
 
     --
     -- requester
     --
-    ---------------------------------------------------------------------------
-    local entity_name = "transport-cables:requester-t" .. tostring(tier)
-    local item = table.deepcopy(data.raw["item"]["wooden-chest"])
-    item.name = entity_name
-    item.icons = { { icon = "__transport-cables__/sprites/icons/requester-t" .. tostring(tier) .. ".png" } }
-    item.icon_size = 64
-    item.icon_mipmaps = 4
-    item.place_result = entity_name
-    item.group = "logistics"
-    item.subgroup = "transport-cables"
-    item.order = "e-t" .. tostring(tier)
-
-    data:extend({ item })
+    entity_name = prefix .. "requester-t" .. tostring(tier)
+    data:extend({ {
+        type = "item",
+        name = entity_name,
+        icon = "__transport-cables__/sprites/icons/requester-t" .. tostring(tier) .. ".png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+        place_result = entity_name,
+        group = "logistics",
+        subgroup = "transport-cables",
+        order = "e-t" .. tostring(tier),
+        stack_size = 50
+    } })
 
     --
     -- underground cable
     --
-    ---------------------------------------------------------------------------
-    local entity_name = "transport-cables:underground-cable-t" .. tostring(tier)
-    local item = table.deepcopy(data.raw["item"]["underground-belt"])
-    item.name = entity_name
-    item.icons = { { icon = "__transport-cables__/sprites/icons/underground-cable-t" .. tostring(tier) .. ".png" } }
-    item.icon_size = 64
-    item.icon_mipmaps = 4
-    item.place_result = entity_name
-    item.group = "logistics"
-    item.subgroup = "transport-cables"
-    item.order = "b-t" .. tostring(tier)
-
-    data:extend({ item })
+    entity_name = prefix .. "underground-cable-t" .. tostring(tier)
+    data:extend({ {
+        type = "item",
+        name = entity_name,
+        icon = "__transport-cables__/sprites/icons/underground-cable-t" .. tostring(tier) .. ".png",
+        icon_size = 64,
+        icon_mipmaps = 4,
+        place_result = entity_name,
+        group = "logistics",
+        subgroup = "transport-cables",
+        order = "b-t" .. tostring(tier),
+        stack_size = 50,
+    } })
 end
