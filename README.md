7c9eed425dc30824a4b333e547a04f12d51778a1
Michael Koller <koller_michael@outlook.de>
2024-01-01 21:46:56 +0100

    add README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# Transport Cables
+
+A [mod](https://mods.factorio.com/) for the video game [Factorio](https://www.factorio.com/).
+
+## How it works
+
+## TODOs
+
+* write the how it works section
+* replace the helper lamps' circuit wire connector sprites with empty sprites
+* use custom circuit wire sprites for the helper lamps
+* rotating a cable should connect it to a new neighbor
+* non-HD sprites
+* using pipette on requester containers should yield a requester
+* when mod entities are destroyed, should only the corresponding circuit network id tables be updated (instead of always calling update_network_ids())?
--- copy_to_factorio.py
+++ copy_to_factorio.py
@@ -5,16 +5,18 @@ import json
 from pathlib import Path
 
 
+source = Path(os.getcwd())
+destination = Path(r"C:\Users\Admin\AppData\Roaming\Factorio\mods")
+folders = ["locale", "prototypes", "sprites"]
+files = ["control.lua", "data.lua", "info.json", "lib.lua"]
+
 with open("info.json", "r") as f:
     version = json.load(f)["version"]
 
-
-source = Path(os.getcwd())
-print("source:", source)
-
-destination = Path(r"C:\Users\Admin\AppData\Roaming\Factorio\mods")
 mod_folder = f"transport-cables_{version}"
 dest = destination / mod_folder
+
+print("source:", source)
 print("destination:", destination / mod_folder)
 
 # delete old transport-cables folders
@@ -25,10 +27,6 @@ for f in glob.glob(str(destination / "transport-cables_*")):
 # make new transport-cables folder
 os.mkdir(destination / mod_folder)
 print("made:", destination / mod_folder)
-
-folders = ["locale", "prototypes", "sprites"]
-files = ["control.lua", "data.lua", "info.json", "lib.lua"]
-
 
 for folder in folders:
     shutil.copytree(source / folder, dest / folder)
